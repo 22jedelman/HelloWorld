487bc641b54acab45c9cb62325848500ad04766c
22jedelman <54331055+22jedelman@users.noreply.github.com>
2019-10-14 11:30:14 -0700

    Updates.

--- main.ts
+++ main.ts
@@ -3,4 +3,5 @@ brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
     brick.setStatusLight(StatusLight.GreenFlash);
     brick.showString("Hello world", 1)
     brick.showString("Everything is awesome", 2)
+
 })
